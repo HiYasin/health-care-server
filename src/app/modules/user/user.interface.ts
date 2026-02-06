d32c65c41ae1dca60586d6a9c5d5b44a99ef10c3
hiyasin <c223059@ugrad.iiuc.ac.bd>
2026-02-07 01:03:07 +0600

    feat(user): create user module and update user model schema. (setup): installed bcrypt, cloudinary, multer

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,8 @@
+export type createPatientInput = {
+    name: string;
+    email: string;
+    password: string;
+    profilePhoto?: string;
+    contactNumber?: string;
+    address?: string;    
+}
---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,29 @@
+import bcrypt from "bcrypt";
+import { createPatientInput } from "./user.interface";
+import config from "../../../config";
+import { prisma } from "../../shared/prisma";
+
+const createPatient = async (payload: createPatientInput) => {
+    const hashedPassword = await bcrypt.hash(payload.password, config.salt_rounds);
+    const result = await prisma.$transaction(async (tnx) => {
+
+        await tnx.user.create({
+            data: {
+                email: payload.email,
+                password: hashedPassword,
+            }
+        });
+
+        return await tnx.patient.create({
+            data: {
+                name: payload.name,
+                email: payload.email,
+            }
+        });
+    });
+
+    return result;
+};
+export const UserService = {
+        createPatient
+};
